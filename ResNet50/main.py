8be37956c53ed91c2b81eda0286431b57b161a43
Fahao Chen <chenfh612@gmail.com>
2022-01-23 01:41:48 +0900

    update

--- ResNet50/main.py
+++ ResNet50/main.py
@@ -18,7 +18,7 @@ from utils import progress_bar
 
 import wandb
 
-wandb.init(project="Trace", entity="fahao")
+wandb.init(project="single", entity="fahao")
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
@@ -47,7 +47,7 @@ transform_test = transforms.Compose([
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=64, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
@@ -61,7 +61,7 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer',
 print('==> Building model..')
 # net = VGG('VGG19')
 # net = ResNet18()
-net = ResNet50()
+net = ResNet152()
 # net = ResNet152()
 # net = PreActResNet18()
 # net = GoogLeNet()
@@ -157,7 +157,7 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+200):
+for epoch in range(start_epoch, start_epoch+1):
     train(epoch)
     test(epoch)
     scheduler.step()
