34ca71b368d8c6862ea2f1d609301e682f1b2680
cfh19980612 <56435427+cfh19980612@users.noreply.github.com>
2022-01-25 15:42:05 +0900

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,15 +1,28 @@
 import os
+import wandb
 
-# ResNet50 task
-print(os.system("cd ResNet50"))
-print(os.system("python main.py"))
+wandb.init(project="Switching", entity="fahao", name="ResNet-Transformer")
 
-# GraphSAGE task
-print(os.system("cd .."))
-print(os.system("cd GraphSAGE"))
-print(os.system("python main.py"))
+# Task 1
+# print(os.system("cd GraphSAGE; python main.py --num-epochs=20"))
+# print(os.system("cd ResNet50; python main.py"))
 
-# # Transformer task
-# print(os.system("cd .."))
-# print(os.system("cd transformer-pytorch-master"))
-# print(os.system("python train.py --problem wmt32k --output_dir ./output --data_dir ./wmt32k_data"))
+# Task 2
+# print(os.system("cd ResNet50; python main.py"))
+# print(os.system("cd GraphSAGE; python main.py --num-epochs=20"))
+ 
+# Task 3 
+print(os.system("cd GraphSAGE; python main.py --num-epochs=20")) 
+print(os.system("cd transformer-pytorch-master; python train.py --problem wmt32k --output_dir ./output --data_dir ./wmt32k_data")) 
+ 
+# Task 4 
+# print(os.system("cd transformer-pytorch-master; python train.py --problem wmt32k --output_dir ./output --data_dir ./wmt32k_data")) 
+# print(os.system("cd GraphSAGE; python main.py --num-epochs=20")) 
+ 
+# Task 5 
+# print(os.system("cd ResNet50; python main.py")) 
+# print(os.system("cd transformer-pytorch-master; python train.py --problem wmt32k --output_dir ./output --data_dir ./wmt32k_data")) 
+ 
+# Task 6 
+# print(os.system("cd transformer-pytorch-master; python train.py --problem wmt32k --output_dir ./output --data_dir ./wmt32k_data")) 
+# print(os.system("cd ResNet50; python main.py")) 
