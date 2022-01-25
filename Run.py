15badd6d442f9fb6c9a7c1bcef94e73c975a5b82
cfh19980612 <56435427+cfh19980612@users.noreply.github.com>
2022-01-25 15:43:22 +0900

    Update Run.py

--- Run.py
+++ Run.py
@@ -4,16 +4,16 @@ import wandb
 wandb.init(project="Switching", entity="fahao", name="ResNet-Transformer")
 
 # Task 1
-# print(os.system("cd GraphSAGE; python main.py --num-epochs=20"))
-# print(os.system("cd ResNet50; python main.py"))
+print(os.system("cd GraphSAGE; python main.py --num-epochs=1"))
+print(os.system("cd ResNet50; python main.py"))
 
 # Task 2
 # print(os.system("cd ResNet50; python main.py"))
 # print(os.system("cd GraphSAGE; python main.py --num-epochs=20"))
  
 # Task 3 
-print(os.system("cd GraphSAGE; python main.py --num-epochs=20")) 
-print(os.system("cd transformer-pytorch-master; python train.py --problem wmt32k --output_dir ./output --data_dir ./wmt32k_data")) 
+# print(os.system("cd GraphSAGE; python main.py --num-epochs=1")) 
+# print(os.system("cd transformer-pytorch-master; python train.py --problem wmt32k --output_dir ./output --data_dir ./wmt32k_data")) 
  
 # Task 4 
 # print(os.system("cd transformer-pytorch-master; python train.py --problem wmt32k --output_dir ./output --data_dir ./wmt32k_data")) 
